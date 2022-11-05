c8c499e9324af18fcc55eeaec2534a044c846498
Roman Smelov <truejaba@protonmail.com>
2022-11-05 23:04:27 +0300

    'version2'

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,29 @@
+import java.nio.charset.Charset;
 public class Main {
-    // Ввод количества ящиков
-    // Метод который делит количество ящиков на количество контейнеров т.е на 27 и выдает необходимое их количество
-    // Метод который принимает количество контейнеров и исходя из их количества выдает необходимое количество грузовиков
-    // Метод который выдает информацию исходя из данных
+
     static int truck = 0;
-    static int boxes = 7;
+    static int boxes = 29;
     static int containers = 0;
+    static int boxesInContainers = 27;
+    static int containersInTruck = 12;
 
     public static void main(String[] args) {
-        for (int i = 0; i <= boxes + 1;) {
-            truck++;
-            if(i == boxes + 1){break;}
-            System.out.println("Грузовик " + truck);
-            while (true) {
-                containers++;
-                if(i == boxes + 1){break;}
-                System.out.println("Контейнер: " + containers);
-                while (true){
-                    i++;
-                    if(i == boxes + 1){break;}
-                    System.out.println("Ящик " + i);
-                    if (i % 3 == 0 ) {break;}
-                }
-                if(containers % 2 == 0){break;}
+        if (boxes > 0){truck = 1; containers = 1;}
+        else {System.out.println("Ничего перевозить не нужно");}
+        for (int i = 1; i <= boxes; i++){
+            if (i == 1) {System.out.println("Грузовик " + truck + "\r\n" +
+                    "\s" + "Контейнер " + containers);}
+            System.out.println("\s\s" + "Ящик " + i);
+            if (boxes == i) {break;}
+            if(i % boxesInContainers == 0) {
+                    if (containers  % containersInTruck == 0) {
+                        truck++;
+                        System.out.println("Грузовик " + truck);}
+                    containers++;
+                    System.out.println("Контейнер " + containers);
             }
         }
+        System.out.println("Нужно грузовиков " + truck + "\r\n" +
+                "Нужно контейнеров " + containers);
     }
-
-
-
-
 }
